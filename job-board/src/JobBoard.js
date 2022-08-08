486c53d6c8e56782e35f75a971ead3a173d0b785
FAANG Tech Leads <107974605+faangtechleads@users.noreply.github.com>
2022-08-08 18:07:13 +0800

    review job board

--- job-board/src/JobBoard.js
+++ job-board/src/JobBoard.js
@@ -1,3 +1,8 @@
+// FTL: By doing this, you're not using CSS modules and the CSS classes in JobBoard.css are defined in the global scope.
+// That may be what you want, but there are better ways of doing it. Since you're using CRA, there's built-in support
+// for CSS modules via webpack.
+// Read up on CSS modules here: https://css-tricks.com/css-modules-part-1-need/
+// Learn how to use it in your React app: https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/
 import "./JobBoard.css";
 import { useEffect, useState } from "react";
 import { fetchJobsMetadata } from "./fetch";
@@ -10,6 +15,11 @@ function JobBoard() {
 
   useEffect(() => {
     async function fetchData() {
+      // FTL: Reading from localStorage can fail in certain situations:
+      // 1. The localStorage value was corrupted (users can change it since it's in the client)
+      // 2. In certain browsers + incognito mode, localStorage is not available.
+      // Hence you should use try/catch whenever you load from localStorage and handle the
+      // error cases gracefully.
       const cached_jobs = JSON.parse(window.localStorage.getItem("hn_jobs"));
 
       let newJobs = [];
@@ -22,6 +32,9 @@ function JobBoard() {
       }
       setJobs(newJobs);
     }
+    // FTL: Since you're not awaiting fetchData to proceed before doing anything else
+    // (there's no line after the next line), there's no need for `.then()`, it doesn't
+    // do anything.
     fetchData().then();
   }, []);
 
@@ -36,19 +49,37 @@ function JobBoard() {
         <div className="title">
           <h1>HN Jobs</h1>
         </div>
+        {/* FTL: For rendering lists, add role="list" and role="listitem" accordingly 
+        if you're not using the semantic HTML tags (ul/li). 
+        https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/list_role
+        */}
         <div className="job-board">
           {jobs.map((job) => {
-            const title = job.title.split(" ").splice(0, 3).join(" ");
-            let description = job.title.split(" ").splice(3).join(" ");
+            // FTL: This way of splitting the title is buggy and only works if the company name is a single world.
+            // You should split using the (YC XXX) token instead. You can use Regex for it.
+            const [title, description] = job.title.split(/\(YC \w+\)/).map(s => s.trim())
             if (description.length >= DESC_CHAR_LIMIT) {
               description = description.substring(0, DESC_CHAR_LIMIT) + "...";
             }
             return (
               <div
-                key={job.title}
-                className={"job-cell"}
+                // FTL: Titles does not make a great key because there's no guarantee
+                // that they are unique. You should use the job.id instead.
+                key={job.id}
+                className="job-cell"
                 onClick={() => {
                   if (job.url) {
+                    // FTL: Avoid using JavaScript to do stuff that the browsers can already
+                    // do. You should make the whole job posting a link instead. A link is
+                    // more accessible (read up about a11y) because:
+                    // 1. Screenreaders know it's a link and will announce that it's a link
+                    // 2. Hovering over a link will show the link URL at the bottom of the browser
+                    // 3. Users can choose to open in new tab if they want. For this qn the requirements state to open in a new tab so this point isn't too relevant
+                    // 4. Users can copy the link by rightclicking on the card
+                    // 5. Links have the pointer cursor styling which tells the user this is a link
+                    // 6. Keyboard users can tab through the items and hit enter to navigate to a link. Divs by default are not focussable. 
+                    //    You can mitigate this by adding tabIndex={0} to your <div> but the aria-role is still wrong and is not as good as using a link in the first place.
+                    //    Currently your list is not usable by keyboard users at all.
                     window.open(job.url, "_blank");
                   } else {
                     window.open(
@@ -62,11 +93,16 @@ function JobBoard() {
                 <div>{description}</div>
                 <div className="job-date-by">
                   <div className="job-date">
+                    {/* There's the <time> tag which is more appropriate
+                      https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
+                    */}
                     {new Date(job.time * 1000).toLocaleDateString()}
                   </div>
                   <div
                     className="user-profile"
                     onClick={() => {
+                      // Like before, don't use JavaScript to redirect if you
+                      // can use HTML.
                       window.open(
                         `https://news.ycombinator.com/user?id=${job.by}`,
                         "_blank"
@@ -79,9 +115,17 @@ function JobBoard() {
               </div>
             );
           })}
+          {/* // FTL: Since this part never ever changes, aka static, you can extract it out outside of your component
+            into the module scope. */}
           {loadingItems.map((idx) => {
+            // FTL: This is really cool! A more common approach (as opposed to blurring) for
+            // loading states of items which contain text is to use rectangles to replace the text instead
+            // something like this: https://dribbble.com/shots/3826035-Loading-States-Shimmer
+            // You'll see this effect on facebook.com if you scroll your feed fast. Blurring is actually quite uncommon.
             return (
-              <div key={`loading=${idx}`} className={"loading-cell"}>
+              // FTL: Not sure if you're aware but the key only needs to be unique among
+              // its siblings. So there's no need to add a prefix. You can just do `key={idx}`
+              <div key={idx} className="loading-cell">
                 <div className="job-title blur">Title (YC2020)</div>
                 <div className="blur">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, ed
@@ -98,9 +142,16 @@ function JobBoard() {
           <button
             className="button"
             onClick={async () => {
+              // FTL: Use camelCase for variables in JavaScript
               const cached_jobs = JSON.parse(
                 window.localStorage.getItem("hn_jobs")
               );
+              // FTL: Your button shouldn't contain that much logic. It'd be
+              // better to move the data fetching into a single module. You just
+              // tell that module what data you need, and the module will return you 
+              // the data without the caller caring or knowing how it's achieved.
+              // In the module it will either fetch from the cache (localStorage)
+              // or make a network request if it doesn't exist.
               if (cursor + MORE_LOADING_SIZE <= cached_jobs.cursor) {
                 setJobs((currentJobs) =>
                   currentJobs.concat(
--- job-board/src/fetch.js
+++ job-board/src/fetch.js
@@ -39,6 +39,33 @@ export const fetchJobsMetadata = async (start, end = []) => {
     };
   }
 
+  // FTL: All the reading from localStorage should be done in this file
+  // to have better encapsulation. Now you are reading the data from multiple files
+  // and it's harder to remember where to make changes related to reading from the cache.
+  // The localStorage key is also repeated in several places of the code. 
+  // It should be extracted out as a constant as well.
+
+  // FTL: I would recommend caching the data in this format instead, an object of jobID to the
+  // job posting data `{[jobID]: data}`.
+  
+  // Note that the question did not ask you to cache the list of IDs, it only asked that 
+  // you cache the result for individual job postings.
+  //
+  // This is because the order of the results could change depending on certain conditions 
+  // (e.g. as time passes, new posts come in. user might sort according to some keywords, popularity, etc)
+  //
+  // However, your current approach caches the entire list which works on first glance but is flawed
+  // because I can never see the most updated data beyond the first load. If I visit your site
+  // a few hours later, I still see the same results because you have cached the entire list + postings
+  // and will never hit the network again. This is fine if your cache has an expiry, but your cache
+  // currently lasts forever.
+
+  // If you cached only the job data, after time passes, you'll still see new postings. 
+  // Assuming a reverse chronological order, when you scroll down far enough, you'll 
+  // reach the older results where you've seen before and this is where the cache comes in useful. 
+  // The job posting data can be read from localStorage instead of fetching over the network. 
+
+  // Let me know if this doesn't makes sense, I can explain further.
   window.localStorage.setItem(
     "hn_jobs",
     JSON.stringify({
